22736634f5af87f9e6ccd8afc77d1b2eba35eda7
adeteyah <adeteyah@gmail.com>
2025-02-04 12:03:16 +0000

    Feb 4, 2025, 7:03 PM

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-svh flex justify-center items-center">
+    <main>
+      <NuxtPage />
+    </main>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+  data() {
+    return {
+      n1: 0,
+      n2: 0,
+      operator: "+",
+      userInput: "",
+    };
+  },
+  created() {
+    this.nextQuestion();
+    this.clearUserInput();
+  },
+  computed: {
+    question() {
+      return `${this.n1} ${this.operator} ${this.n2}`;
+    },
+  },
+  methods: {
+    clearUserInput() {
+      this.userInput = "";
+    },
+    nextQuestion() {
+      this.clearUserInput();
+      this.n1 = this.getRandomNumber();
+      this.n2 = this.getRandomNumber();
+    },
+    getRandomNumber() {
+      return Math.floor(Math.random() * 10) + 1;
+    },
+    checkAnswer() {
+      if (this.userInput == this.n1 + this.n2) {
+        this.nextQuestion();
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="flex gap-2">
+    <div>{{ question }}</div>
+    <input
+      class="w-[3ch]"
+      type="text"
+      v-model="userInput"
+      v-on:keyup.enter="checkAnswer"
+      autofocus
+    />
+  </div>
+</template>
